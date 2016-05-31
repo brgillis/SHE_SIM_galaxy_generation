b74e5e450bf21a3a7d1b1f9ec16232615be04a10
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2016-05-31 13:03:07 +0100

    Some updates to tests
    Former-commit-id: 1d9f03918b847d2b74bbcb5a9bb57bf21bd90ba4

--- SHE_SIM_galaxy_image_generation/tests/python/test_import.py
+++ SHE_SIM_galaxy_image_generation/tests/python/test_import.py
@@ -1,17 +1,27 @@
+"""
+    Tests of various imports, to make sure galsim is installed and other parts of the project are
+    linked correctly.
+"""
+
 import pytest
 
 
 class TestCase(unittest.TestCase):
-    """
 
-
-    """
+    # External imports
 
     def testGalSimImport(self):
         import galsim
+        
+    # Internal python imports
 
-    def testSHESIMImport(self):
-        import SHE_SIM
+    def testicebrgpyImport(self):
+        import icebrgpy
+        
+    # Internal C++ imports
 
     def testcIceBRGpyImport(self):
         import cIceBRGpy
+
+    def testSHESIMImport(self):
+        import SHE_SIM
