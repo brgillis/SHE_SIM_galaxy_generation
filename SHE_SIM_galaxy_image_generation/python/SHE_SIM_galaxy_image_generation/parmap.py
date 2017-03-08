1678fdf1473a7596b7c660bd52112fe1748c446a
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-03-08 17:34:50 +0100

    Restoring file that went missing forom repo
    Former-commit-id: 29ce5628c29170453c48975b94b9cf392d525549

---
+++ SHE_SIM_galaxy_image_generation/python/SHE_SIM_galaxy_image_generation/parmap.py
@@ -0,0 +1,50 @@
+""" @file /disk2/brg/Program_Files/workspace/SBE_Example_Code/SHE_SBE_common/parmap.py
+
+    Created 31 Aug 2015
+
+    Better implementation of Pool.map, using code provided by klaus se at
+    http://stackoverflow.com/a/16071616/5099457
+
+    ---------------------------------------------------------------------
+
+    Copyright (C) 2015 Bryan R. Gillis
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import multiprocessing
+
+def fun(f,q_in,q_out):
+    while True:
+        i,x = q_in.get()
+        if i is None:
+            break
+        q_out.put((i,f(x)))
+
+def parmap(f, X, nprocs = multiprocessing.cpu_count()):
+    q_in   = multiprocessing.Queue(1)
+    q_out  = multiprocessing.Queue()
+
+    proc = [multiprocessing.Process(target=fun,args=(f,q_in,q_out)) for _ in range(nprocs)]
+    for p in proc:
+        p.daemon = True
+        p.start()
+
+    sent = [q_in.put((i,x)) for i,x in enumerate(X)]
+    [q_in.put((None,None)) for _ in range(nprocs)]
+    res = [q_out.get() for _ in range(len(sent))]
+
+    [p.join() for p in proc]
+
+    return [x for i,x in sorted(res)]
